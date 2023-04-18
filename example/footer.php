a3456004cae9f1052bdaf10ae34cde45032b0961
Mike Welsh <dev@kytschi.com>
2023-04-18 08:44:45 +0100

    keep forgetting to update the example

--- example/footer.php
+++ example/footer.php
@@ -1,9 +1,13 @@
-<footer>
-    <div class="footer-item">
-        <img src="/assets/dumbdog.png" alt="dumb-dog">
-        <a href="https://github.com/kytschi/dumb-dog" target="_blank">Powered by Dumb Dog</a>
-    </div>
-    <div class="footer-item float-right">
-        <a href="/sitemap">sitemap</a>
-    </div>
-</footer>
+            <footer>
+                <div class="footer-item">
+                    <img src="/assets/dumbdog.png" alt="dumb-dog">
+                    <a href="https://github.com/kytschi/dumb-dog" target="_blank">Powered by Dumb Dog</a>
+                </div>
+                <div class="footer-item float-right">
+                    <a href="/sitemap" class="button"><span>sitemap</span></a>
+                    <a href="/privacy" class="button"><span>privacy</span></a>
+                </div>
+            </footer>
+        </main>
+    </body>
+</html>
