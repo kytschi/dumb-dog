03881d03605166b58ea32ad76d5fdae0ce1c0a3f
Mike Welsh <dev@kytschi.com>
2023-04-18 08:44:02 +0100

    changed the variable naming to single one, easier

--- example/footer.php
+++ example/footer.php
@@ -3,4 +3,7 @@
         <img src="/assets/dumbdog.png" alt="dumb-dog">
         <a href="https://github.com/kytschi/dumb-dog" target="_blank">Powered by Dumb Dog</a>
     </div>
+    <div class="footer-item float-right">
+        <a href="/sitemap">sitemap</a>
+    </div>
 </footer>
